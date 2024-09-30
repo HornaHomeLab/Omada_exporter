8252c9b2e9d5f99069d130b9a60140ded326945d
Stanisław Horna <stanislawhorna@outlook.com>
2024-09-30 22:07:53 +0200

    add span_id to logs

--- app/src/Observability/Log/SpanFormatter.py
+++ app/src/Observability/Log/SpanFormatter.py
@@ -4,9 +4,18 @@ from loki_logger_handler.loki_logger_handler import LoggerFormatter
 
 class SpanFormatter(LoggerFormatter):
     def format(self, record):
-        trace_id = trace.get_current_span().get_span_context().trace_id
+        current_span = trace.get_current_span()
+        trace_id = current_span.get_span_context().trace_id
+        span_id = current_span.get_span_context().span_id
+
         if trace_id == 0:
             record.trace_id = None
         else:
             record.trace_id = "{trace:32x}".format(trace=trace_id)
+
+        if span_id == 0:
+            record.span_id = None
+        else:
+            record.span_id = "{span:16x}".format(span=span_id)
+
         return super().format(record)
--- app/src/Observability/Log/logger.py
+++ app/src/Observability/Log/logger.py
@@ -4,17 +4,18 @@ from src.Observability.Log.SpanFormatter import SpanFormatter
 import src.Config as Config
 
 
-
-logger = logging.getLogger("uvicorn")
-logger.setLevel(Config.LOG_LEVEL)
-
 if Config.USE_LOKI:
+    logger = logging.getLogger()
     loki_handler = LokiLoggerHandler(
         url=f"http://{Config.LOKI_IP}:{Config.LOKI_PORT}/loki/api/v1/push",
         labels={
             "service_name": Config.SERVICE_NAME,
             "Environment": Config.ENVIRONMENT
-            },
+        },
         defaultFormatter=SpanFormatter()
     )
     logger.addHandler(loki_handler)
+else:
+    logger = logging.getLogger("uvicorn")
+
+logger.setLevel(Config.LOG_LEVEL)
